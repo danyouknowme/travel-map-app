c0142ad0bf2a2ef9e8f31e73e1e35a7437c9d6e0
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-02 16:56:43 +0700

    connect database by mongoDB

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection with MongoDB is successfully."))
+  .catch(err => console.log(err));
+
+app.listen(8080, () => {
+  console.log("Backed server is started at port 8080");
+});
