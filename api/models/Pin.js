a8442b9b6dd7b63e1ba445c1fffbe5c5444a0760
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-02 17:03:04 +0700

    create the models

---
+++ api/models/Pin.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const PinSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, min: 3, max: 20, unique: true },
+    title: { type: String, required: true, max: 3 },
+    desc: { type: String, required: true, min: 3 },
+    rating: { type: Number, required: true, min: 0, max: 5 },
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("Pin", PinSchema);
